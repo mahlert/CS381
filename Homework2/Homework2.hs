6e515bc563b3560b92f52dd974fff6b6ec1ff838
christopherpredriau <perdriac@oregonstate.edu>
2020-04-24 10:57:36 -0700

    made some changes and fixed some stuff

--- Homework2/Homework2.hs
+++ Homework2/Homework2.hs
@@ -1,11 +1,11 @@
 --2a: define the abstract synaxt for the above languge as a Haskell data type
-type circuit = (gates, links)
-type gates = [(Int, gateFn)]
-data gateFn = and | or | xor | not
-type links = [link]
-data link = L (Int, Int) (Int, Int)
+data Pair = Pair Int Int
+data Circuit = Circuit Gates Links
+data Gates = Gate Int GateFn Gates | GNone
+data GateFn = And | Or | Xor | Not
+data Links = Link Pair Pair Links | LNone
 
---2b: represent the half adder circuit in abstract syntax,
---that is,
+--2b: represent the half adder circuit in abstract syntax, that is,
 --as a Haskell data type
-([[1, xor], [2, and]], [L, (1,1) (2,1), L (1,2) (2,2)])
+halfaddr = Circuit (Gate 1 Xor (Gate 2 And GNone))
+                   (Link (Pair 1 1) (Pair 2 1) (Link (Pair 1 2) (Pair 2 2) LNone))
