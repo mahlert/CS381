4675becd49e43dcea1e1eb311025ed7a53e938e0
Thomas Mahler <mahlert@oregonstate.edu>
2020-05-03 16:18:32 -0700

    sem' function added.

--- Homework3/exercise2.hs
+++ Homework3/exercise2.hs
@@ -9,13 +9,14 @@ type Line = (Int, Int, Int, Int)
 type Lines = [Line]
 
 semS :: Cmd -> State -> (State, Lines)
---Change Pen Mode
 semS (Pen p) (x,y,z) = ((p,y,z),[(y,z,y,z)])
---MoveTo
 semS (MoveTo a b) (x,y,z) = ((x,a,b), l1)
      where l1 = [(y,z,a,b)]
---Sequence Cmd
 semS (Seq c c') (x,y,z) = (s2,l3)
      where (s1,l1) = semS c (x,y,z)
      	   (s2,l2) = semS c' s1
 	   l3 = l1 ++ l2
+
+sem' :: Cmd -> Lines
+sem' (x) = l1
+     where (s1,l1) = semS x (Up,0,0)
