be89787f44d7f1f9665d7882bf2ce30ff0c57642
Thomas Mahler <mahlert@flip1.engr.oregonstate.edu>
2020-05-03 09:49:05 -0700

    Mode and MoveTo Semantics added.

---
+++ Homework3/test.hs
@@ -0,0 +1,17 @@
+data Cmd = Pen Mode
+	 | MoveTo Int Int
+	 | Seq Cmd Cmd
+	 deriving Show
+
+data Mode = Up | Down deriving Show
+type State = (Mode, Int, Int)
+type Line = (Int, Int, Int, Int)
+type Lines = [Line]
+
+semS :: Cmd -> State -> (State, Lines)
+--Base Case 
+--Change Pen Mode
+semS (Pen p) (x,y,z) = ((p,y,z),[(y,z,y,z)])
+--MoveTo
+semS (MoveTo a b) (x,y,z) = ((x,y,z), [(y,z,a,b)])
+--Sequence Cmd (Recurvsive call?)
