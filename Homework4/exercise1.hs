b75a04c03aae7adca8c58cefc3ce37f6ad05cfdc
Jaelyn-Litzinger <litzingj@oregonstate.edu>
2020-05-12 18:07:32 -0700

    finished ex 1

--- Homework4/exercise1.hs
+++ Homework4/exercise1.hs
@@ -1,3 +1,5 @@
+-- Exercise 1 part a --
+
 type Prog = [Cmd]
 
 data Cmd = LD Int | ADD | MULT | DUP | INC | SWAP | POP Int deriving Show
@@ -25,8 +27,11 @@ rank (p:ps) r | under >= 0       = rank ps (under + adds)
                     under        = r - subs
 rank _ _      = Nothing
 
+-- Exercise 1 Part b --
+
 type Stack = [Int]
 type D = Stack -> Stack
+data St = A Stack deriving Show
 
 sem :: Prog -> D
 sem [] y    = y
@@ -41,8 +46,12 @@ semCmd INC     stack = (last stack)+1:(drop 1 stack)
 semCmd SWAP    stack = ((last . init) stack):(last stack):(drop 2 stack)
 semCmd (POP i) stack = (drop i stack)
 
+typeCorrect :: Prog -> Bool
+typeCorrect e = rankP e /= Nothing
 
---semStatTC :: Prog ->
+semStatTC :: Prog -> Maybe Stack
+semStatTC p | typeCorrect p = Just(sem p [])
+            | otherwise     = Nothing
 
 
 
