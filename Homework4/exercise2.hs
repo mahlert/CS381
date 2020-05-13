3d88e34681b47587db1fcf4296b319c2dc5e2ab8
christopherpredriau <perdriac@oregonstate.edu>
2020-05-13 13:48:24 -0700

    think I got 2a, added test cases to the bottom

--- Homework4/exercise2.hs
+++ Homework4/exercise2.hs
@@ -5,11 +5,29 @@ data Shape = X
 
 type BBox = (Int,Int)
 
-data Type = Int | BBox | TypeError
-	  deriving (Eq,Show)
+--data Type = Int | BBox | TypeError
+--	  deriving (Eq,Show)
 
-bbox :: Shape -> Type
-bbox X 				   		= Int
-bbox (TD s1 s2) | bbox s1==Int && bbox s2==Int 	= BBox
-bbox (LR s1 s2) | bbox s1==Int && bbox s2==Int	= BBox
-bbox _						= TypeError
+--bbox :: Shape -> Type
+--bbox X 				   		= Int
+--bbox (TD s1 s2) | bbox s1==Int && bbox s2==Int 	= BBox
+--bbox (LR s1 s2) | bbox s1==Int && bbox s2==Int	= BBox
+--bbox _			= TypeError
+
+
+bbbox :: Shape -> BBox
+bbox X = (1, 1)
+bbox (TD s1 s2) | s1i >= s2i = (s1i, s1j + s2j)
+                 | s1i < s2i  = (s2i, s1j + s2j)
+                 where (s1i, s1j) = bbox s1
+                       (s2i, s2j) = bbox s2
+
+bbox (LR s1 s2) | s1j >= s2i = (s1i + s2i, s1j)
+                 | s1j < s2i  = (s1i + s2i, s2j)
+                 where (s1i, s1j) = bbox s1
+                       (s2i, s2j) = bbox s2
+
+
+--test case: bbbox(LR(TD X (LR X X)) X) should return (3,2)
+--test case: bbbox (LR (TD (TD X X) X) X) should return (2,3)
+
