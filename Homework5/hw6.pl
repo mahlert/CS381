ad322551928d596f5e90940a621634cd3ad3886c
Jaelyn-Litzinger <litzingj@oregonstate.edu>
2020-06-01 22:22:35 -0700

    fixed a, b, c, started on d exercise 1

--- Homework5/hw6.pl
+++ Homework5/hw6.pl
@@ -20,12 +20,13 @@ enroll(john,381).
 enroll(jim,399).
 
 /* part a of exercise 1 */
-schedule(S, P, T) :- when(X, T), where(X, R).
+schedule(S, P, T) :- enroll(S, C), when(C, T), where(C, P).
 /* part b of exercise 1 */
-useage(R, T) :- when(x, T), where(X, R).
+useage(R, T) :- when(X, T), where(X, R).
 /*part c of exercise 1 */
 conflict(C1, C2) :- where(C1, X), where(C2, X), when(C1, Y), when(C2, Y), C1\=C2.
-
+/*part d of exercise 1 */
+meet(A, B) :- enroll(A, X), enroll(B, X), A\=B; enroll(A, X), enroll(B, Y), when(X, T1), when(Y, T2), where(X, P), where(Y, P), A\=B, T1==T2+1;
 
 /* Exercise 2 */
 
